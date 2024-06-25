44b0bf455bea20d49c748e8df9226dbeddcacff6
zstio-zeromski <zeromski.czest@gmail.com>
2024-06-25 18:54:12 +0200

    dodawanie wpisow

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,4 +1,16 @@
+use std::cell::RefCell;
+
+thread_local!{
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
 fn greet(name: String, last_name: i8) -> String {
     format!("Hello, {} {}!", name, last_name)
+}
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String){
+    WPISY.with(|wpisy:&RefCell<Vec<String>> |{
+        wpisy.borrow_mut().push(wpis)
+    });
 }
