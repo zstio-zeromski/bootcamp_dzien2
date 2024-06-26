03c4a684af30185d8d6f403efe0ef8795efd726e
zstio-zeromski <zeromski.czest@gmail.com>
2024-06-26 19:53:27 +0200

    usun i edytuj wpis

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -8,6 +8,9 @@
             <div v-for="(wpis, index) in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
                 <p>id: {{ index }}</p>
                 <p>{{ wpis }}</p>
+                <button class="bg-blue-600 rounded-md text-white p-4" @click="deleteWpis(index)">usun</button> 
+                <input class="border-2 border-blue-600 p-4" v-model="nowyWpis" type="text"> 
+                <button class="bg-blue-600 rounded-md text-white p-4" @click="editWpis(index, this.nowyWpis)">edytuj</button> 
             </div>
         </div>
         <div class="flex justify-center flex-col">
@@ -30,14 +33,23 @@ import { dzien2_backend } from 'declarations/dzien2_backend/index';
         methods: {
             async dodajWpisy(){
                 await dzien2_backend.dodaj_wpis(this.nowyBlog);
+                await this.pobierzWpisy();
             },
             async pobierzWpisy(){
                 this.wpisy = await dzien2_backend.odczytaj_wpisy();
+            }, 
+            async deleteWpis(index){
+                await dzien2_backend.usun_wpis(index);
+                await this.pobierzWpisy();
+            },
+            async editWpis(index, text){
+                await dzien2_backend.edytuj_wpis(index, text);
+                await this.pobierzWpisy();
+            },        
+            //uruchomienie wpisow na starcie
+            async mounted(){
+                this.pobierzWpisy()
             }
-        },
-        //uruchomienie wpisow na starcie
-        async mounted(){
-            this.pobierzWpisy()
         }
     }
 </script>
